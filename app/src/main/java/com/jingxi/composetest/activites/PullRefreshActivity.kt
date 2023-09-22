55e1fa9e07a7e31fdf784849b9bb2ded0abc05ab
s_tree <344096470@qq.com>
2023-09-22 17:54:40 +0800

    PullRefreshActivity

--- app/src/main/java/com/jingxi/composetest/activites/PullRefreshActivity.kt
+++ app/src/main/java/com/jingxi/composetest/activites/PullRefreshActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,6 +15,7 @@ import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -28,44 +30,49 @@ class PullRefreshActivity:ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            swipeRefreshLayout()
+            val loadingState = remember {
+                mutableStateOf(false)
+            }
+            swipeRefreshLayout(
+                loadingState = loadingState,
+                onRefresh = {
+                    /**
+                     * 保持在加载状态
+                     */
+                    loadingState.value = true
+                }){
+                LazyColumn(
+                    Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()){
+                    items(count = 30){
+                        Text(text = "$it",
+                            fontSize = 26.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(80.dp),
+                            textAlign = TextAlign.Center)
+                    }
+
+                }
+            }
         }
     }
 
     @OptIn(ExperimentalMaterialApi::class)
     @Composable
-    fun swipeRefreshLayout(){
-        val refreshingState = remember {
-            mutableStateOf(false)
-        }
-
+    fun swipeRefreshLayout(loadingState: State<Boolean>,onRefresh: () -> Unit,content: @Composable BoxScope.() -> Unit){
         val pullRefreshState = rememberPullRefreshState(
-            refreshing = refreshingState.value,
-            onRefresh = {
-                refreshingState.value = true
-                print("in refresh")
-            })
+            refreshing = loadingState.value,
+            onRefresh = onRefresh)
         Box(
             contentAlignment = Alignment.TopCenter,
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
                 .pullRefresh(pullRefreshState)) {
-            LazyColumn(
-                Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()){
-                items(count = 30){
-                    Text(text = "$it",
-                        fontSize = 26.sp,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(80.dp),
-                        textAlign = TextAlign.Center)
-                }
-
-            }
-            PullRefreshIndicator(refreshing = refreshingState.value, state = pullRefreshState)
+            content()
+            PullRefreshIndicator(refreshing = loadingState.value, state = pullRefreshState)
         }
     }
 }
